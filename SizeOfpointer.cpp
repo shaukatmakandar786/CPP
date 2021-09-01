019f532a7774a85896ee43b1b08b5a2df96fc4a9
shaukatmakandar786 <shaukatmakandar786@gmail.com>
2021-09-01 08:07:08 +0530

    size of pointer variable in cpp

---
+++ SizeOfpointer.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int *p1;
+    char *p2;
+    float *p3;
+    cout<<"size of int pointer variable is "<<sizeof(p1)<<endl;
+    cout<<"size of char pointer variable is "<<sizeof(p2)<<endl;
+    cout<<"size of float pointer variable is "<<sizeof(p3)<<endl;
+    return 0;
+}
