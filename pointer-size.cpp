6fbe3f454f28ca6d2a4f414d9227a4a9ddcd7dca
Shaukat Makandar <39257815+shaukatmakandar786@users.noreply.github.com>
2022-04-05 08:50:13 +0530

    Create pointer-size.cpp

---
+++ pointer-size.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int *p=NULL;
+    float *p1=NULL;
+    char *c=NULL;
+    
+    cout<<sizeof(p);
+    cout<<sizeof(p1);
+    cout<<sizeof(c);
+
+    return 0;
+}
+
+// 888
