ac2f2572128ea6075aa6c10a099ce5dbcf56d2ef
Shaukat Makandar <39257815+shaukatmakandar786@users.noreply.github.com>
2022-04-16 22:22:09 +0530

    Create 01_.cpp

---
+++ interview_questions/01_.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+class A1
+{
+    public:
+    void f1(){
+        cout<<"A1-f1()";
+    }
+};
+class A2
+{
+    public:
+    void f1(){
+        cout<<"A2-f1()";
+    }
+};
+class B: public A1,public A2{
+    public:
+};
+int main()
+{
+    B o;
+    o.f1();
+}
+
+// error: request for member ‘f1’ is ambiguous
