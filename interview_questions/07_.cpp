e6bbbf24f45c74b1a1a5f142858f6dd6308ffcf5
Shaukat Makandar <39257815+shaukatmakandar786@users.noreply.github.com>
2022-04-16 22:56:16 +0530

    Create 07_.cpp

---
+++ interview_questions/07_.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+class A1
+{
+    public:
+   virtual void f1(){
+        cout<<"A1-f1()"<<endl;;
+    }
+};
+class A2
+{
+    public:
+    void f1(int x){
+        cout<<"A2-f1()"<<endl;;
+    }
+};
+class B: public A1,public A2{
+    public:
+   void f1(){
+        cout<<"B-f1()"<<endl;;
+    }
+};
+int main()
+{
+    A1 *p;
+    p=new B;
+    p->f1();
+}
+
+// B-f1()
