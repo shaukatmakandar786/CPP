c27fd49a0255a95074d823c16daaeec4a237db9f
Shaukat Makandar <39257815+shaukatmakandar786@users.noreply.github.com>
2022-04-18 22:46:22 +0530

    Create 16_.cpp

---
+++ interview_questions/16_.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+
+using namespace std;
+
+class A
+{
+    int *p;
+    public:
+    A()
+    {
+            p=new int;
+            *p=10;
+    }
+    ~A()
+    {
+            delete p;
+           cout<<"destroy location pointed by p\n";
+    }
+};
+
+class B:public A
+{
+    int *q;
+    public:
+    B()
+    {
+            q=new int;
+            *q=20;
+    }
+    ~B()
+    {
+            delete q;
+           cout<<"destroy location pointed by q\n";
+    }
+};
+void fun()
+{
+    A *ptr=new B;
+    delete ptr;// here compiler is doing Eearly binding thats why direct parent destructor is called,Because ptr is type of A
+}
+int main()
+{
+   fun();
+   
+    return 0;
+}
+// destroy location pointed by p
