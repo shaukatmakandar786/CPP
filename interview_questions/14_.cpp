e82f00030c8f4408d2f86558bfbd21f8db5ed291
Shaukat Makandar <39257815+shaukatmakandar786@users.noreply.github.com>
2022-04-18 21:44:28 +0530

    Create 14_.cpp

---
+++ interview_questions/14_.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+class A1
+{
+    public:
+    void f1(){
+        cout<<"A1-f1()";
+    }
+};
+class A2
+{
+    public:
+    void f1(){
+        cout<<"A2-f1()";
+    }
+};
+class B: public A1,public A2{
+    public:
+   
+};
+int main()
+{
+    B o;
+    o.A1::f1();
+   
+}
+// A1-f1()
