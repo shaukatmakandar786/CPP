6582c36d4c738f4fe28626edffb8d599a2ff65dd
Shaukat Makandar <39257815+shaukatmakandar786@users.noreply.github.com>
2022-04-27 15:46:15 +0530

    Create arraySTL.cpp

---
+++ STL/arraySTL.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<array>
+
+using  namespace std;
+int main()
+{
+    array <int,5> a={11,22,33,44,55};
+    array <int,5> a1={1,2,3,4,5};
+
+    cout<<a.at(2)<<endl;
+    cout<<a[4]<<endl;
+    cout<<a.front()<<endl;
+    cout<<a.back()<<endl;
+
+    a.fill(10);
+    for (size_t i = 0; i < 5; i++)
+    {
+        cout<<a[i]<<"  ";
+    }
+     cout<<endl;
+    a.swap(a1);
+    for (size_t i = 0; i < 5; i++)
+    {
+        cout<<a[i]<<"  ";
+    }
+    cout<<endl;
+
+    for (size_t i = 0; i < 5; i++)
+    {
+        cout<<a1[i]<<"  ";
+    }
+
+    cout<<"\nsize is "<<a.size()<<endl;
+    return 0;
+}
+
+
+// 33
+// 55
+// 11
+// 55
+// 10  10  10  10  10  
+// 1  2  3  4  5  
+// 10  10  10  10  10  
+// size is 5
